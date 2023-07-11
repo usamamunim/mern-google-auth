e6d4aceba5bfce09ea24ef5a5e2b6c1d8dbc5678
usamamunim <usama.munim48@gmail.com>
2023-07-11 19:00:32 +0500

    Tailwindcss setup

--- src/components/pages/authentication/Auth.js
+++ src/components/pages/authentication/Auth.js
@@ -1,11 +1,11 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+const Auth = () => {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </div>
   );
-}
+};
 
-export default App;
+export default Auth;
